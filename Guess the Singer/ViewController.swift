a48241915619ae331c39b5f1da91d0d415d7984e
Testing_author <testing_beta@mail.com>
2020-01-26 13:01:09 +0200

    Created the initial values for "Singers" and set the value at "0" for score. Created function "askQuestion" for when a user is to be proposed with variants to choose from. Added borderLayout and borderColor for the images.

--- Guess the Singer/ViewController.swift
+++ Guess the Singer/ViewController.swift
@@ -14,11 +14,31 @@ class ViewController: UIViewController {
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     
+    var singers = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        singers += ["drake", "ed_sheeran", "eminem", "james_blunt", "jay_sean", "lil_wayne", "marron_5", "selena_gomez", "taylor_swift"]
+        
+        // setting the borderWidth of the tables containing the Singer pictures
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        // setting the borderColor of the tables containing the Singer pictures, cgColor added in order for such to understand it, since it belongs to another CA layer that does not recognize what a color is
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
     }
 
-
+    func askQuestion() {
+        button1.setImage(UIImage(named: singers[0]), for: .normal)
+        button2.setImage(UIImage(named: singers[1]), for: .normal)
+        button3.setImage(UIImage(named: singers[2]), for: .normal)
+    }
 }
 
